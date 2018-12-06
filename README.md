970b0feb22f6c164ee3ed142278a134bcd286e38
hmrbll <45451716+hmrbll@users.noreply.github.com>
2018-12-06 22:23:16 +0300

    Development (#3)
    * Changes I made so far
    
    * Break
    
    * Comments
    
    * Break
    
    * Complete Code
    
    * Comments
    
    * Update README.md
    
    * Create CodeBook.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Getting and Cleaning Data Course Project
+
+The goal of this project is to collect and clean a data set.
+
+UCI HAR Dataset: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## Explanation of run_analysis.R
+
+This R script gets inputs from data set. Features of training and test sets, activity names, subjects of training and test sets. Creates subset of original data set with average and standart deviation data. Modifies column names to be more clean for explatory analysis. And as an output run_analysis.R script creates output.txt file which consists of averages of each measurement in subset for each subject and activity.
--- run_analysis.R
+++ run_analysis.R
@@ -1,43 +1,90 @@
+#Assign directory of data files to a variable
 data_files_directory <- "UCI HAR Dataset"
-
+#Get feature names
 feature_names_path <- paste(data_files_directory, "features.txt", sep = "/")
 feature_names <- read.table(feature_names_path, stringsAsFactors = FALSE)
-
+#Get measurements of training set
 X_train_path <- paste(data_files_directory, "train", "X_train.txt", sep = "/")
 X_train <- read.table(X_train_path, stringsAsFactors = FALSE)
-
+#Assign feature names to measurements of training set
 colnames(X_train) <- feature_names[[2]]
-
+#Get measurements of test set
 X_test_path <- paste(data_files_directory, "test", "X_test.txt", sep = "/")
 X_test <- read.table(X_test_path, stringsAsFactors = FALSE)
-
+#Assign feature names to measurements of test set
 colnames(X_test) <- feature_names[[2]]
-
+#Get activities of training set
 y_train_path <- paste(data_files_directory, "train", "y_train.txt", sep = "/")
 y_train <- read.table(y_train_path, stringsAsFactors = FALSE)
 y_train <- as.factor(y_train$V1)
-
+#Get activity labels
 activity_labels_path <- paste(data_files_directory, "activity_labels.txt", sep = "/")
 activity_labels <- read.table(activity_labels_path, stringsAsFactors = FALSE)
-
+#Insert activity labels 
 levels(y_train) <- activity_labels[[2]]
-
+#Combine training set into single data set
 train <- cbind(X_train, y_train)
 colnames(train)[[ncol(train)]] <- "Activity"
-
+#Get activities of test set
 y_test_path <- paste(data_files_directory, "test", "y_test.txt", sep = "/")
 y_test <- read.table(y_test_path, stringsAsFactors = FALSE)
 y_test <- as.factor(y_test$V1)
-
+#Insert activity labels
 levels(y_test) <- activity_labels[[2]]
-
+#Combine test set into single data set
 test <- cbind(X_test, y_test)
 colnames(test)[[ncol(test)]] <- "Activity"
-
+#Combine training and test sets
 all_data <- rbind(train, test)
-
+#Remove remaining files
 rm(list=ls()[!ls() %in% "all_data"])
-
+#Select mean and standart deviation measurements
 only_mean_and_std <- grepl('-(mean|std)\\(', colnames(all_data))
 selector <- c(which(only_mean_and_std), ncol(all_data))
 selected_data <- all_data[selector]
+#Redefine data set directory
+data_files_directory <- "UCI HAR Dataset"
+#Get subject from train set
+subject_train_path <- paste(data_files_directory, "train", "subject_train.txt", sep = "/")
+subject_train <- read.table(subject_train_path, stringsAsFactors = FALSE)
+#Get subject from test set
+subject_test_path <- paste(data_files_directory, "test", "subject_test.txt", sep = "/")
+subject_test <- read.table(subject_test_path, stringsAsFactors = FALSE)
+#Combine training and test subjects
+subject <- rbind(subject_train, subject_test)
+#Combine subject with main and selected data sets
+all_data <- cbind(subject, all_data)
+selected_data <- cbind(subject, selected_data)
+#Add column names 
+colnames(all_data)[[1]] <- "subject"
+colnames(selected_data)[[1]] <- "subject"
+#Remove unnecessary files
+rm(list=ls()[!ls() %in% "selected_data"])
+#Get mean columns
+means <- grepl('mean\\(', colnames(selected_data))
+#Change column names
+for(i in 1:length(colnames(selected_data))) {
+        if(means[i]) {
+                colnames(selected_data)[i] <- paste("Average", colnames(selected_data)[i], sep = " ")
+        }
+}
+colnames(selected_data) <- gsub("-mean\\(\\)", "", colnames(selected_data))
+#Get std columns
+standart_deviations <- grepl('std\\(', colnames(selected_data))
+#Change column names
+for(i in 1:length(colnames(selected_data))) {
+        if(standart_deviations[i]) {
+                colnames(selected_data)[i] <- paste("Standart Deviation", colnames(selected_data)[i], sep = " ")
+        }
+}
+colnames(selected_data) <- gsub("-std\\(\\)", "", colnames(selected_data))
+#Change column names with Time and Frequency
+colnames(selected_data) <- gsub("(^.+) t", "\\1 Time ", colnames(selected_data))
+colnames(selected_data) <- gsub("(^.+) f", "\\1 Frequency", colnames(selected_data))
+#Average each measurement in terms of subject and activity
+library('dplyr')
+output <- selected_data %>%
+        group_by(subject,Activity) %>%
+        summarise_all(funs(mean))
+#Write output to a txt file
+write.table(output,row.name = FALSE,file = "tidy_data_set.txt")
