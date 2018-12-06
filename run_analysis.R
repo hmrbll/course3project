bf647dd351457aede862a87085aac1269bc3101f
hmrbll <huseyinemre_balli@hotmail.com>
2018-12-06 17:27:27 +0300

    Changes I made so far

--- run_analysis.R
+++ run_analysis.R
@@ -12,3 +12,15 @@ X_test_path <- paste(data_files_directory, "test", "X_test.txt", sep = "/")
 X_test <- read.table(X_test_path, stringsAsFactors = FALSE)
 
 colnames(X_test) <- feature_names[[2]]
+
+y_train_path <- paste(data_files_directory, "train", "y_train.txt", sep = "/")
+y_train <- read.table(y_train_path, stringsAsFactors = FALSE)
+y_train <- as.factor(y_train$V1)
+
+activity_labels_path <- paste(data_files_directory, "activity_labels.txt", sep = "/")
+activity_labels <- read.table(activity_labels_path, stringsAsFactors = FALSE)
+
+levels(y_train) <- activity_labels[[2]]
+
+train <- cbind(X_train, y_train)
+colnames(train)[[ncol(train)]] <- "Activity"
